1e2be4298d0c6d5530812fae8715188725fa8df4
CubeStorm <cubestorm72@gmail.com>
2021-12-06 19:42:39 +0100

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <span> Hello ToDo App! </span>
+        </div>
+    );
+}
+
+export default App;
